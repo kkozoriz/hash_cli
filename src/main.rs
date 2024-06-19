d91d447895a7884b2d15591d4a5163cf5ef88d5b
kkozoriz <kkozoriz7@yandex.ru>
2024-06-19 23:53:53 +0300

    feat!: cli added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::Parser;
+use hash_cli::found_hash;
+
+#[derive(Parser, Debug)]
+#[command(name = "hash-cli")]
+#[command(version = "0.1.0")]
+#[command(about = "Command line arguments for hash-cli", long_about = None)]
+pub struct Args {
+    #[arg(short = 'N', default_value_t = 0)]
+    zero_count: usize,
+
+    #[arg(short = 'F', default_value_t = 1)]
+    hash_count: usize,
+}
+
+struct Cli {
+    args: Args,
+}
+
+impl Cli {
+    fn new() -> Cli {
+        Cli {
+            args: Args::parse(),
+        }
+    }
+    fn run(self) {
+        found_hash(self.args.zero_count, self.args.hash_count)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    Cli::new().run()
 }
