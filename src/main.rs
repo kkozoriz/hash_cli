3e8d23a9d6f9040d7aa7a6faed792abe86aa9838
kkozoriz <kkozoriz7@yandex.ru>
2024-06-20 16:51:27 +0300

    refactor: remove structs from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,5 @@
-use clap::Parser;
-use hash_cli::found_hash;
-
-#[derive(Parser, Debug)]
-#[command(name = "hash-cli")]
-#[command(version = "0.1.0")]
-#[command(about = "Command line arguments for hash-cli", long_about = None)]
-pub struct Args {
-    #[arg(short = 'N', default_value_t = 0)]
-    zero_count: usize,
-
-    #[arg(short = 'F', default_value_t = 1)]
-    hash_count: usize,
-}
-
-struct Cli {
-    args: Args,
-}
-
-impl Cli {
-    fn new() -> Cli {
-        Cli {
-            args: Args::parse(),
-        }
-    }
-    fn run(self) {
-        found_hash(self.args.zero_count, self.args.hash_count)
-    }
-}
+mod cli;
+use crate::cli::Cli;
 
 fn main() {
     Cli::new().run()
